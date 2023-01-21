d98d169b0d6dd3bda4be02d0cecf0b6c73ea40f3
LUSIASX <93148560+LUSIASX@users.noreply.github.com>
2023-01-21 17:34:01 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -6039,11 +6039,132 @@ Server:AddTextBox({
 })
 
 Server:AddButton({
-	Name = "Join JobServer",
+	Name = "Join JobId",
 	Callback = function()
 		game.TeleportService:TeleportToPlaceInstance(game.placeId,_G.JobId, game.Players.LocalPlayer)
 	end
 })
+Server:AddButton({
+	Name = "Copy JobId",
+	Callback = function()
+		setclipboard(tostring(game.JobId))
+	end
+})
+Server:AddButton({
+	Name = "Hop Server",
+	Callback = function()
+		Hop()
+	end
+})
+
+local ts = game:GetService("TeleportService")
+
+local p = game:GetService("Players").LocalPlayer
+
+Server:AddButton({
+	Name = "Rejoin",
+	Callback = function()
+		ts:Teleport(game.PlaceId, p)
+	end
+})
+function Hop()
+    local PlaceID = game.PlaceId
+    local AllIDs = {}
+    local foundAnything = ""
+    local actualHour = os.date("!*t").hour
+    local Deleted = false
+    local File =
+        pcall(
+            function()
+                AllIDs = game:GetService("HttpService"):JSONDecode(readfile("NotSameServers.json"))
+            end
+        )
+    if not File then
+        table.insert(AllIDs, actualHour)
+        writefile("NotSameServers.json", game:GetService("HttpService"):JSONEncode(AllIDs))
+    end
+    function TPReturner()
+        local Site
+        if foundAnything == "" then
+            Site =
+                game.HttpService:JSONDecode(
+                    game:HttpGet(
+                        "https://games.roblox.com/v1/games/" .. PlaceID .. "/servers/Public?sortOrder=Asc&limit=100"
+                    )
+                )
+        else
+            Site =
+                game.HttpService:JSONDecode(
+                    game:HttpGet(
+                        "https://games.roblox.com/v1/games/" ..
+                        PlaceID .. "/servers/Public?sortOrder=Asc&limit=100&cursor=" .. foundAnything
+                    )
+                )
+        end
+        local ID = ""
+        if Site.nextPageCursor and Site.nextPageCursor ~= "null" and Site.nextPageCursor ~= nil then
+            foundAnything = Site.nextPageCursor
+        end
+        local num = 50
+        for i, v in pairs(Site.data) do
+            local Possible = true
+            ID = tostring(v.id)
+            if tonumber(v.maxPlayers) > tonumber(v.playing) then
+                for _, Existing in pairs(AllIDs) do
+                    if num ~= 0 then
+                        if ID == tostring(Existing) then
+                            Possible = false
+                        end
+                    else
+                        if tonumber(actualHour) ~= tonumber(Existing) then
+                            local delFile =
+                                pcall(
+                                    function()
+                                        delfile("NotSameServers.json")
+                                        AllIDs = {}
+                                        table.insert(AllIDs, actualHour)
+                                    end
+                                )
+                        end
+                    end
+                    num = num + 50
+                end
+                if Possible == true then
+                    table.insert(AllIDs, ID)
+                    wait()
+                    pcall(
+                        function()
+                            writefile("NotSameServers.json", game:GetService("HttpService"):JSONEncode(AllIDs))
+                            wait()
+                            game:GetService("TeleportService"):TeleportToPlaceInstance(
+                            PlaceID,
+                            ID,
+                            game.Players.LocalPlayer
+                            )
+                        end
+                    )
+                    wait(50)
+                end
+            end
+        end
+    end
+
+    function Teleport()
+        while wait() do
+            pcall(
+                function()
+                    TPReturner()
+                    if foundAnything ~= "" then
+                        TPReturner()
+                    end
+                end
+            )
+        end
+    end
+
+    Teleport()
+end
+
 
 function topos(Pos)
     Distance = (Pos.Position - game.Players.LocalPlayer.Character.HumanoidRootPart.Position).Magnitude
